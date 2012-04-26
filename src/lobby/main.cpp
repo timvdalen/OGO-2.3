86c86caf92c64a406f85246773223c55fb8daec6
Ferry <f.timmers@student.tue.nl>
2012-04-26 12:38:48 +0200

    lobby source opzet

--- src/lobby/main.cpp
+++ src/lobby/main.cpp
@@ -1,11 +1,21 @@
 /*
  * Lobby main file
  *
+ * Authors: Ogo 2.3 Group 3 2012
+ * 
+ * Date: 26-04-12 12:11
+ *
+ * Description: Player accumulation and social interaction prior to a game.
+ *
  */
 
 #include <stdlib.h>
+
+//------------------------------------------------------------------------------
 
 int main(int argc, char *argv[])
 {
 	return (EXIT_SUCCESS);
 }
+
+//------------------------------------------------------------------------------
