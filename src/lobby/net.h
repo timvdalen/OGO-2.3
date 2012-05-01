2fb0119bb98191d04bc829767244c4760cd2f9f4
Ferry <f.timmers@student.tue.nl>
2012-05-01 15:11:52 +0200

    game projects stub and lobby net abstractions

--- src/lobby/net.h
+++ src/lobby/net.h
@@ -10,9 +10,45 @@
 #ifndef _NET_H
 #define _NET_H
 
-//------------------------------------------------------------------------------
+namespace Net {
 
 //------------------------------------------------------------------------------
+
+void Initialize();
+void Terminate();
+
+//------------------------------------------------------------------------------
+
+struct Address
+{
+	Address();
+	Address(unsigned long address = 0, unsigned port = 0);
+	Address(const char *address, unsigned int port = 0);
+	
+	unsigned long debug();
+	
+	private:
+	size_t length;
+	void *data;
+};
+
+struct Socket
+{
+	Address local;
+	Address remote;
+};
+
+struct UDPSocket : public Socket
+{
+};
+
+struct TCPSocket : public Socket
+{
+};
+
+//------------------------------------------------------------------------------
+
+} // namespace Net
 
 #endif // _NET_H
 
