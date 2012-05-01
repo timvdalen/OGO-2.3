bc98221399d6226690ab7d2e51de45523da1031c
KAYLukas <kay.lukas@gmail.com>
2012-05-01 17:22:47 +0200

    Class diagram deleted

---
+++ src/lobby/Lobby/Lobby/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Lobby
+//
+//  Created by Kay Lukas on 01-05-12.
+//  Copyright (c) 2012 TU/e. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
