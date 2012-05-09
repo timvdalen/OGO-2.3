41efa30af120c9ffa6b07d7403940df858668659
Ferry <f.timmers@student.tue.nl>
2012-05-09 13:35:33 +0200

    core code

--- src/game/core.cpp
+++ src/game/core.cpp
@@ -4,5 +4,41 @@
 
 #include "core.h"
 
+namespace Core {
+
 //------------------------------------------------------------------------------
+
+void Object::preRender()
+{
+	// Stack has only at least 32 levels! Maybe just translate twice...
+	glPushMatrix();
+	glTranslated(origin.x, origin.y, origin.z);
+}
+
+//------------------------------------------------------------------------------
+
+void Object::render()
+{
+	preRender();
+	
+	// Nothing to render, but will render its children next
+	
+	postRender();
+}
+
+//------------------------------------------------------------------------------
+
+void Object::postRender()
+{
+	set<ObjectHandle>::iterator it;
+	for (it = children.begin(); it != children.end(); ++it)
+		(*it)->render();
+	
+	glPopMatrix();
+}
+
+//------------------------------------------------------------------------------
+
+} // namespace Core
+
 //------------------------------------------------------------------------------
