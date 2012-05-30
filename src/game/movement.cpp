b5fe115782b79454ba81d9583b7d602154a41720
Ferry Timmers <ferry.timmers@student.tue.nl>
2012-05-30 17:12:41 +0200

    movement class added

---
+++ src/game/movement.cpp
@@ -0,0 +1,103 @@
+/*
+ * Movement module -- see header file for more info
+ */
+
+#include "movement.h"
+
+namespace Movement {
+
+//------------------------------------------------------------------------------
+
+void Controller::moveX(Direction dir)
+{
+	if (dir == dirLeft)
+	{
+	}
+	else if (dir == dirRight)
+	{
+	}
+	else // Key released
+	{
+	}
+}
+
+//------------------------------------------------------------------------------
+
+void Controller::moveY(Direction dir)
+{
+	if (dir == dirForward)
+	{
+	}
+	else if (dir == dirBackward)
+	{
+	}
+	else // Key released
+	{
+	}
+}
+
+//------------------------------------------------------------------------------
+
+void Controller::moveZ(Direction dir)
+{
+	if (dir == dirUp)
+	{
+	}
+	else if (dir == dirDown)
+	{
+	}
+	else // Key released
+	{
+	}
+}
+
+//------------------------------------------------------------------------------
+
+void Controller::lookX(Direction dir)
+{
+	if (dir == dirLeft)
+	{
+	}
+	else if (dir == dirRight)
+	{
+	}
+	else // Key released
+	{
+	}
+}
+
+//------------------------------------------------------------------------------
+
+void Controller::lookY(Direction dir)
+{
+	if (dir == dirForward) // Zoom in
+	{
+	}
+	else if (dir == dirBackward) // Zoom out
+	{
+	}
+	else // Key released
+	{
+	}
+}
+
+//------------------------------------------------------------------------------
+
+void Controller::lookZ(Direction dir)
+{
+	if (dir == dirUp)
+	{
+	}
+	else if (dir == dirDown)
+	{
+	}
+	else // Key released
+	{
+	}
+}
+
+//------------------------------------------------------------------------------
+
+} // namespace Movement
+
+//------------------------------------------------------------------------------
---
+++ src/game/movement.h
@@ -0,0 +1,52 @@
+/*
+ * Movement module
+ *
+ * Date: 30-05-12 16:44
+ *
+ * Description:
+ */
+
+#ifndef _MOVEMENT_H
+#define _MOVEMENT_H
+
+#include "video.h"
+
+//! Movement module
+//! Takes care of camera movement
+namespace Movement {
+
+using namespace Video;
+
+//------------------------------------------------------------------------------
+
+//! Camera controller
+class Controller
+{
+	public:
+	Camera &camera; //! Assigned camera
+	
+	enum Direction
+	{
+		dirDown    = -1, dirLeft  = -1, dirBackward = -1,
+		dirNeutral =  0,
+		dirUp      =  1, dirRight =  1, dirForward  =  1
+	};
+	
+	Controller(Camera &C) : camera(C) {}
+	
+	void moveX(Direction); //! Move camera left and right
+	void moveY(Direction); //! Move camera forward and backwards
+	void moveZ(Direction); //! Move camera up and down
+	
+	void lookX(Direction); //! Look (rotate camera) left and right
+	void lookY(Direction); //! Zoom in and out
+	void lookZ(Direction); //! Look (rotate camera) up and down
+};
+
+//------------------------------------------------------------------------------
+
+} // namespace Movement
+
+#endif // _MOVEMENT_H
+
+//------------------------------------------------------------------------------
