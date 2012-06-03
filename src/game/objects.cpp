4b340927070388b45e625b5a5de6da2f0035f2a8
Tim van Dalen <Tim@timvdalen.nl>
2012-06-03 18:37:56 +0200

    World bounds fix; width en height werden verdubbeld.

--- src/game/objects.cpp
+++ src/game/objects.cpp
@@ -19,32 +19,36 @@ ObjectHandle BoundedObject::checkCollision(Point<double> origin, Vector<double> 
 void World::draw(){
 	#define HIGH 500
 
+	double halfWidth = width/2;
+	double halfHeight = height/2;
+
+
 	glBegin(GL_TRIANGLES);
 		glTexCoord2d(0, 0);
-		glVertex3f(-1 * width, -1 * height, 0);
+		glVertex3f(-1 * halfWidth, -1 * halfHeight, 0);
 		glTexCoord2d(1, 0);
-		glVertex3f(-1 * width, height, 0);
+		glVertex3f(-1 * halfWidth, halfHeight, 0);
 		glTexCoord2d(0.5, 1);
 		glVertex3f(0, 0, HIGH);
 
 		glTexCoord2d(0, 0);
-		glVertex3f(width, height, 0);
+		glVertex3f(halfWidth, halfHeight, 0);
 		glTexCoord2d(1, 0);
-		glVertex3f(width, -1* height, 0);
+		glVertex3f(halfWidth, -1* halfHeight, 0);
 		glTexCoord2d(0.5, 1);
 		glVertex3f(0, 0, HIGH);
 
 		glTexCoord2d(0, 0);
-		glVertex3f(-1 * width, height, 0);
+		glVertex3f(-1 * halfWidth, halfHeight, 0);
 		glTexCoord2d(1, 0);
-		glVertex3f(width, height, 0);
+		glVertex3f(halfWidth, halfHeight, 0);
 		glTexCoord2d(0.5, 1);
 		glVertex3f(0, 0, HIGH);
 
 		glTexCoord2d(0, 0);
-		glVertex3f(width, -1 * height, 0);
+		glVertex3f(halfWidth, -1 * halfHeight, 0);
 		glTexCoord2d(1, 0);
-		glVertex3f(-1 * width, -1 * height, 0);
+		glVertex3f(-1 * halfWidth, -1 * halfHeight, 0);
 		glTexCoord2d(0.5, 1);
 		glVertex3f(0, 0, HIGH);
 		
