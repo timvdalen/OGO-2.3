0ecd757acf2a906a47bc019be34440eade91138e
Tim van Dalen <Tim@timvdalen.nl>
2012-06-10 23:04:02 +0200

    NPC toegevoegd om wat multiplayer features te showen voordat netwerk af is.

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -39,6 +39,10 @@ map<Button,Direction> lookbind;
 map<Button,word> lookcount;
 FPS fps;
 ObjectHandle cube;
+
+ObjectHandle *npc = NULL;
+int lastmess = 0;
+
 bool building = false;
 bool lastview = false;
 
@@ -101,6 +105,8 @@ int main(int argc, char *argv[])
 	playercube->material = ShadedMaterial(Cf(1,0,0,1));
 	player->children.insert(playercube);
 	player->rotation = Rd(0,Vd(0,0,1));
+	
+	npc = new ObjectHandle(Objects::Player(1, 'b', "NPC", Pd(30, 40, 0)));
 
 	cube->material = Assets::Test;
 
@@ -115,6 +121,7 @@ int main(int argc, char *argv[])
 	}
 
 	world->children.insert(player);
+	world->children.insert(*npc);
 
 	v1.world = world;
 
@@ -197,6 +204,79 @@ void Frame()
 	
 	Objects::Player * player = TO(Objects::Player,controller->player);
 	player->update(controller->camera.objective);
+	
+	int time = Video::ElapsedTime();
+	Objects::Player * pNPC = TO(Objects::Player, *npc);
+	World *w = TO(World, controller->world);
+	HUD *h = TO(HUD, w->hud);
+	if(time > 900 && time < 1100){
+		if(lastmess == 0){
+			ChatMessage m = ChatMessage(*pNPC, "Hey man, welcome to the game!");
+			h->messageDisplayer->addMessage(m);
+			lastmess++;
+		}
+	}else if(time > 2900 && time < 3100){
+		if(lastmess == 1){
+			ChatMessage m = ChatMessage(*pNPC, "So your name is " + player->name + "?");
+			h->messageDisplayer->addMessage(m);
+			lastmess++;
+		}
+	}else if(time > 4900 && time < 5100){
+		if(lastmess == 2){
+			ChatMessage m = ChatMessage(*pNPC, "Try to look around with the mouse");
+			h->messageDisplayer->addMessage(m);
+			lastmess++;
+		}
+	}else if(time > 6900 && time < 7100){
+		if(lastmess == 3){
+			ChatMessage m = ChatMessage(*pNPC, "If you press W you will roll in the direction");
+			ChatMessage m2 = ChatMessage(*pNPC, "you're looking in");
+			h->messageDisplayer->addMessage(m);
+			h->messageDisplayer->addMessage(m2);
+			lastmess++;
+		}
+	}else if(time > 8900 && time < 9100){
+		if(lastmess == 4){
+			ChatMessage m = ChatMessage(*pNPC, "Left clicking will make you shoot a laser beam");
+			h->messageDisplayer->addMessage(m);
+			lastmess++;
+		}
+	}else if(time > 10900 && time < 11100){
+		if(lastmess == 5){
+			ChatMessage m = ChatMessage(*pNPC, "Pressing B will enter build mode");
+			h->messageDisplayer->addMessage(m);
+			lastmess++;
+		}
+	}else if(time > 12900 && time < 13100){
+		if(lastmess == 6){
+			ChatMessage m = ChatMessage(*pNPC, "Try to build some towers");
+			h->messageDisplayer->addMessage(m);
+			lastmess++;
+		}
+	}else if(time > 13900 && time < 14100){
+		if(lastmess == 7){
+			ChatMessage m = ChatMessage(*pNPC, "They're of a special stealth");
+			ChatMessage m2 = ChatMessage(*pNPC, "kind that looks like a box.");
+			h->messageDisplayer->addMessage(m);
+			h->messageDisplayer->addMessage(m2);
+			lastmess++;
+		}
+	}else if(time > 16900 && time < 17100){
+		if(lastmess == 8){
+			ChatMessage m = ChatMessage(*pNPC, "Anyway. Have fun!");
+			h->messageDisplayer->addMessage(m);
+			lastmess++;
+		}
+	}else if(time > 19900 && time < 20100){
+		if(lastmess == 9){
+			ChatMessage m = ChatMessage(*pNPC, "Oh, one last thing. You can press [enter] if");
+			ChatMessage m2 = ChatMessage(*pNPC, "you ever want to talk to me!");
+			h->messageDisplayer->addMessage(m);
+			h->messageDisplayer->addMessage(m2);
+			lastmess++;
+		}
+	}
+	
 
 	cube->rotation = cube->rotation * Rd(.1, Vd(0,0,1));
 	controller->look.reset();
@@ -353,6 +433,11 @@ static void getInput(string input){
 	if(!input.empty()){
 		ChatMessage m = ChatMessage(*p, input);
 		h->messageDisplayer->addMessage(m);
+		if(input == "hi"){
+			Objects::Player * pNPC = TO(Objects::Player, *npc);
+			ChatMessage m = ChatMessage(*pNPC, "Hey man, what's up?");
+			h->messageDisplayer->addMessage(m);
+		}
 	}
 
 	//Not so nice, but better than the old way
