2428092578620d126557cdaf275e14eedb233d3a
KAYLukas <kay.lukas@gmail.com>
2012-06-12 09:10:44 +0200

    Added memset to movement
    Bool arrays are not initialized to false. This fixes a bug.

--- src/game/movement.cpp
+++ src/game/movement.cpp
@@ -380,8 +380,10 @@ void Controller::avoidPulverizebyBuilding(){
     int xlength = (int) (w->width / GRID_SIZE);
     int ylength = (int) (w->height / GRID_SIZE);
     bool ** containsBuilding = new bool*[xlength];
-	for (int x = 0; x < xlength; x++) containsBuilding[x] = new bool[ylength];
-    //memset(containsBuilding, 0, sizeof containsBuilding);
+	for (int x = 0; x < xlength; x++){
+        containsBuilding[x] = new bool[ylength];
+        memset(containsBuilding[x], 0, sizeof containsBuilding[x]);
+    }
     //create a mapping with all used grids
     multimap<GridPoint, ObjectHandle>::iterator it;
     for(it = t->structures.begin(); it != t->structures.end(); it++){
