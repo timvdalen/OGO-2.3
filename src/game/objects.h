138b0e0c4ce0d1158b53e7b00f5358755d00f470
Tim van Dalen <Tim@timvdalen.nl>
2012-06-18 22:02:39 +0200

    BoundingBox::BoundingBox(lbl, rth) toegevoegd.

--- src/game/objects.h
+++ src/game/objects.h
@@ -50,6 +50,10 @@ struct BoundingBox
 	            Pd _lbh = Pd(), Pd _rbh = Pd(), Pd _lth = Pd(), Pd _rth = Pd())
 		: lbl(_lbl), rbl(_rbl), ltl(_ltl), rtl(_rtl),
 		  lbh(_lbh), rbh(_rbh), lth(_lth), rth(_rth) {}
+	
+	BoundingBox(Pd _lbl, Pd _rth)
+		: lbl(_lbl), rbl(0), ltl(0), rtl(0),
+		  lbh(0), rbh(0), lth(0), rth(_rth) {}
 };
 
 //------------------------------------------------------------------------------
